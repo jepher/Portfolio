487100995210fb546514b495d6cc1f2ecd214f64
jepher <jeffreyyang217@gmail.com>
2020-05-23 22:52:48 -0400

    Updates

---
+++ precache-manifest.cf69ef6234fb83e5918e74e338e6d333.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4249830f9c282f84a5922dfac5ebf9",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c402965b095923988306",
+    "url": "/Portfolio/static/css/3.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "b3400b97d8c7dde16df2",
+    "url": "/Portfolio/static/css/main.bdde557a.chunk.css"
+  },
+  {
+    "revision": "9ee2186d5e25aa40db01",
+    "url": "/Portfolio/static/js/0.00c5b901.chunk.js"
+  },
+  {
+    "revision": "27fe79f60ce6f9d2665ded19c8398b1c",
+    "url": "/Portfolio/static/js/0.00c5b901.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c402965b095923988306",
+    "url": "/Portfolio/static/js/3.4515d7ee.chunk.js"
+  },
+  {
+    "revision": "f94d41575256abd0b3df7058f7d7e0a4",
+    "url": "/Portfolio/static/js/3.4515d7ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2aeb60913194812e00df",
+    "url": "/Portfolio/static/js/4.68f88f9e.chunk.js"
+  },
+  {
+    "revision": "f98b93a77ad5ca6a77dd",
+    "url": "/Portfolio/static/js/5.82abd1a0.chunk.js"
+  },
+  {
+    "revision": "3981366e046c52ca5905",
+    "url": "/Portfolio/static/js/6.51cfccbf.chunk.js"
+  },
+  {
+    "revision": "60de0dc41a69d2ae9069",
+    "url": "/Portfolio/static/js/7.4f0ff348.chunk.js"
+  },
+  {
+    "revision": "47c36fd3821d3bda9170",
+    "url": "/Portfolio/static/js/8.624b3abc.chunk.js"
+  },
+  {
+    "revision": "f70ca60b28d9ddee7e6d",
+    "url": "/Portfolio/static/js/9.95f85d29.chunk.js"
+  },
+  {
+    "revision": "b3400b97d8c7dde16df2",
+    "url": "/Portfolio/static/js/main.085e0681.chunk.js"
+  },
+  {
+    "revision": "236a3e41e1e3254cca37",
+    "url": "/Portfolio/static/js/runtime-main.e7a4062a.js"
+  },
+  {
+    "revision": "b5f984d1c21970b20048461157228e26",
+    "url": "/Portfolio/static/media/Exo2-Regular.b5f984d1.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/Portfolio/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/Portfolio/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/Portfolio/static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "c641d3e90c64fcb82f8f5e2ac5317e98",
+    "url": "/Portfolio/static/media/Orbitron-VariableFont_wght.c641d3e9.ttf"
+  },
+  {
+    "revision": "77fa00996ecb4104c7880b8749c7c4e0",
+    "url": "/Portfolio/static/media/Righteous-Regular.77fa0099.ttf"
+  },
+  {
+    "revision": "ed1d014bf2b8b72f27d6af65c69f710a",
+    "url": "/Portfolio/static/media/TitilliumWeb-Regular.ed1d014b.ttf"
+  },
+  {
+    "revision": "a9520dd3037eb63a127d38635cb24f12",
+    "url": "/Portfolio/static/media/android_studio_icon.a9520dd3.png"
+  },
+  {
+    "revision": "774c6cff096571e13e496ed9d170f3fd",
+    "url": "/Portfolio/static/media/arm1.774c6cff.png"
+  },
+  {
+    "revision": "1e7f1faf04b8cd03cd6cd5202a924342",
+    "url": "/Portfolio/static/media/arm2.1e7f1faf.png"
+  },
+  {
+    "revision": "39afb9a691d65a9ca469ae2b1c88a30e",
+    "url": "/Portfolio/static/media/avatar.39afb9a6.png"
+  },
+  {
+    "revision": "d6ebf2fa03b94b8d2ca1a161ebbd022b",
+    "url": "/Portfolio/static/media/c_sharp_icon.d6ebf2fa.png"
+  },
+  {
+    "revision": "b408c67816ee64ace6a95489b66b61ce",
+    "url": "/Portfolio/static/media/css_icon.b408c678.png"
+  },
+  {
+    "revision": "72e2316c9544e2ad8efdbb5597f5f462",
+    "url": "/Portfolio/static/media/decision_tree.72e2316c.JPG"
+  },
+  {
+    "revision": "43d94aafc68ef805f1e97642b8caf78e",
+    "url": "/Portfolio/static/media/dio.43d94aaf.png"
+  },
+  {
+    "revision": "de69f68b25642224564c3c928f1ae4b3",
+    "url": "/Portfolio/static/media/dio_game_1.de69f68b.JPG"
+  },
+  {
+    "revision": "3861869cd86a84f425b9ed3f6ef6f713",
+    "url": "/Portfolio/static/media/dio_game_2.3861869c.JPG"
+  },
+  {
+    "revision": "948cfb2e340a505583dafaefb301cf57",
+    "url": "/Portfolio/static/media/git_icon.948cfb2e.png"
+  },
+  {
+    "revision": "1a78c477955559595a33449d39c49d7a",
+    "url": "/Portfolio/static/media/html_icon.1a78c477.png"
+  },
+  {
+    "revision": "a6aeba7cbc93c17ee2ee0b810bd8c274",
+    "url": "/Portfolio/static/media/icomoon.a6aeba7c.ttf"
+  },
+  {
+    "revision": "635d1936a455e622239871aaf7df1cd0",
+    "url": "/Portfolio/static/media/ivi_lab.635d1936.jpg"
+  },
+  {
+    "revision": "3b2ccfedfa868a8032212f9e3a3d7dd6",
+    "url": "/Portfolio/static/media/java_icon.3b2ccfed.png"
+  },
+  {
+    "revision": "c9f5e82ad9a24dad087493d11095bc55",
+    "url": "/Portfolio/static/media/javascript_icon.c9f5e82a.png"
+  },
+  {
+    "revision": "cdd515ab5ac7a3c16f102a5d2100cf9f",
+    "url": "/Portfolio/static/media/mongodb_icon.cdd515ab.png"
+  },
+  {
+    "revision": "9bc26c78ffaf9ac0119a0c577623b062",
+    "url": "/Portfolio/static/media/node_icon.9bc26c78.png"
+  },
+  {
+    "revision": "918cf5ff7254b83c3b21e48a1c5f1a89",
+    "url": "/Portfolio/static/media/profile_picture.918cf5ff.jpg"
+  },
+  {
+    "revision": "37d60abba56b01de81f787b4351b88e9",
+    "url": "/Portfolio/static/media/python_icon.37d60abb.png"
+  },
+  {
+    "revision": "32f7f406b5e2c48df2efbf4c92111c59",
+    "url": "/Portfolio/static/media/rain_alert.32f7f406.png"
+  },
+  {
+    "revision": "bbc94f3df9311bc48ec112f6be82dff1",
+    "url": "/Portfolio/static/media/rain_alert_1.bbc94f3d.JPG"
+  },
+  {
+    "revision": "8d19be0d2e995c539d3b35a2ad125571",
+    "url": "/Portfolio/static/media/rain_alert_2.8d19be0d.JPG"
+  },
+  {
+    "revision": "8e91b489baea7f31c3079339cac04f3b",
+    "url": "/Portfolio/static/media/react_icon.8e91b489.png"
+  },
+  {
+    "revision": "8b59b2b924641d6a45adfe18bf156ca8",
+    "url": "/Portfolio/static/media/tf_icon.8b59b2b9.png"
+  },
+  {
+    "revision": "4a2d93fdc01fda1bc4eb693036abe923",
+    "url": "/Portfolio/static/media/traffic_model.4a2d93fd.JPG"
+  },
+  {
+    "revision": "163870e4307689fb538827e684b01b57",
+    "url": "/Portfolio/static/media/unity_icon.163870e4.png"
+  },
+  {
+    "revision": "6896b4b60cdcd339bb234ee9ca079d69",
+    "url": "/Portfolio/static/media/wog_game_1.6896b4b6.JPG"
+  },
+  {
+    "revision": "8ba8e77065ef00795b0f1a49da4aca50",
+    "url": "/Portfolio/static/media/wog_game_2.8ba8e770.JPG"
+  },
+  {
+    "revision": "2e66035caf647ec935f7a3895ed3e647",
+    "url": "/Portfolio/static/media/word_of_god.2e66035c.png"
+  }
+]);
